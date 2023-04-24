f6c2429081c6119d716c85e2dcbb19076cff05f4
romanunr02 <romanunr02@gmail.com>
2023-04-25 00:41:50 +0300

    generate templates

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -1,5 +1,3 @@
-
-
 const API_ENDPOINT = 'https://restcountries.com/v3.1/'
 const FIELDS = '?fields=name,capital,region,flags,population'
 
@@ -16,18 +14,42 @@ async function getAllCountries() {
 
 getAllCountries().then((countries) => {
     console.log(countries[0])
-    createTemplate(countries[0])
+    countries.slice(0, 10).forEach(createTemplate)
 })
 
 function createTemplate(country) {
-    const template = document.getElementById('country-template') as HTMLTemplateElement
+    const template = document.getElementById('country-template')
     const main = document.getElementsByTagName('main')[0]
-    const content = template.content.cloneNode(true) 
+    const content = template.content.cloneNode(true)
+
+    //image
     const image = content.querySelector('img')
     if (image) {
         image.src = country.flags.png
         image.alt = country.flags.alt
     }
 
+    //name
+    const name = content.querySelector('.country-name')
+    name.innerText = country.name.common
+
+    //population
+    setValueForLabel(content, '#population-', country.name.common, country.population)
+
+    //region
+    setValueForLabel(content, '#region-', country.name.common, country.population)
+
+    //capital
+    setValueForLabel(content, '#capital-', country.name.common, country.capital[0])
+
     main.append(content)
+}
+
+function setValueForLabel(content, id, idSuffix, givenValue) {
+    const value = content.querySelector(id)
+    const label = value.previousElementSibling
+    const newId = value.id + idSuffix
+    label.htmlFor = newId
+    value.id = newId
+    value.innerText = givenValue
 }

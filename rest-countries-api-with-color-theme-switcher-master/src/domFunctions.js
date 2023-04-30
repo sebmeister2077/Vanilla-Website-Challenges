4fd26dca94f3ce1e6f30a301c21e1c8664d7e4a2
romanunr02 <romanunr02@gmail.com>
2023-04-30 22:53:27 +0300

    added dom method to create a single country template

--- rest-countries-api-with-color-theme-switcher-master/src/domFunctions.js
+++ rest-countries-api-with-color-theme-switcher-master/src/domFunctions.js
@@ -71,3 +71,59 @@ export function initializeAutocompleteList() {
         countryNamesList.append(option)
     })
 }
+
+export function createSingleTemplate(country) {
+    const template = document.getElementById('single-country')
+    const main = document.querySelector('.countries')
+    //content is a document fragment, it is not equal to the html dom element
+    const content = template.content.cloneNode(true)
+
+    const anchor = content.querySelector('a')
+    anchor.href = `${location.origin}${location.pathname}`
+    anchor.onclick = function (e) {
+        e.preventDefault()
+        history.pushState(null, '', this.href)
+    }
+    //image
+    const imageFlags = content.querySelectorAll('img')
+    imageFlags.forEach((flag) => {
+        flag.src = country.flags.png
+        flag.alt = country.flags.alt
+    })
+
+    //name
+    const name = content.querySelector('.country-name')
+    name.innerText = country.name.common
+
+    const nativeName = country.name.nativeName[Object.keys(country.name.nativeName)[0]].common
+    createSpecific(content, `single-country-${nativeName}`, nativeName, 'Native Name')
+
+    createSpecific(content, `single-country-${country.population}`, country.population, 'Population')
+    createSpecific(content, `single-country-${country.region}`, country.region, 'Region')
+    createSpecific(content, `single-country-${country.subregion}`, country.subregion, 'Sub Region')
+    createSpecific(content, `single-country-${country.capital.join(',')}`, country.capital.join(','), 'Capital')
+    createSpecific(content, `single-country-${country.tld.join(',')}`, country.tld.join(','), 'Top Level Domain')
+
+    const currencies = Object.keys(country.currencies)
+        .map((k) => country.currencies[k].name)
+        .join(',')
+    createSpecific(content, `single-country-${currencies}`, currencies, 'Currencies')
+    const languages = Object.keys(country.languages)
+        .map((k) => country.languages[k])
+        .join(',')
+    createSpecific(content, `single-country-${languages}`, languages, 'Languages')
+
+    main.append(content)
+}
+function createSpecific(content, id, value, label) {
+    const countrySpecificsContainer = content.querySelector('.country-specifics')
+    const countrySpecificTemplate = content.getElementById('country-specific')
+    const specific = countrySpecificTemplate.content.cloneNode(true)
+    const specificLabel = specific.querySelector('label')
+    specificLabel.for = id
+    specificLabel.innerText = label + ':'
+    const specifivValue = specific.querySelector('span')
+    specifivValue.innerText = value
+    specifivValue.id = id
+    countrySpecificsContainer.append(specific)
+}

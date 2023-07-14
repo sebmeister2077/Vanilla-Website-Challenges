5b0ae6b9179d734945b4325f39a76e5d54c2c562
romanunr02 <romanunr02@gmail.com>
2023-07-14 23:08:14 +0300

    added sqlite

---
+++ htmx/server/db.mjs
@@ -0,0 +1,9 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+export async function getDb() {
+    return open({
+        filename: '/tmp/database.db',
+        driver: sqlite3.Database,
+    })
+}

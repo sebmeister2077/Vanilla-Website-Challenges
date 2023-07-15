0205783e08f421dfb6e42505d28e9745adfd5d38
romanunr02 <romanunr02@gmail.com>
2023-07-15 22:28:44 +0300

    some changes to add and get todos

--- htmx/server/index.js
+++ htmx/server/index.js
@@ -5,6 +5,9 @@ import { getDb } from './db.js'
 const app = express()
 app.use(express.static('public'))
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 getDb().then((db) => db.migrate())
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -19,15 +22,37 @@ app.get('/todo', async (req, res) => {
     const db = await getDb()
     const todos = await db.all('SELECT * FROM Todos')
     res.send(/*html*/ `
-        <div class="flex w-screen items-center justify-center flex-col gap-6">${todos.map(
+    <form hx-boost='true' hx-post="/todo" hx-target="ul" hx-swap='beforeend' class="flex w-screen items-center justify-center flex-col gap-6">
+        <ul>
+        ${todos.map(
             (t) => /*html*/ `
-                <div>${t.text}</div>
+            <li>${t.text} id:${t.id}</div>
             `,
-        )}</div>
+        )}
+        </ul>
+        <input  type="text" value='hello' name='newtodo' class="bg-gunmetal"/>
+        <button>Add todo</button>
+    </form>
         `)
 })
-app.post('/todo', (req, res) => {})
+app.post('/todo', async (req, res) => {
+    const db = await getDb()
+    const statement = await db.prepare('INSERT INTO Todos("text") VALUES (@text);')
+    const { newtodo } = req.body
+    await statement.run({ '@text': newtodo })
+    const newEntry = await db.get('SELECT * FROM Todos ORDER BY id DESC LIMIT 1;')
+    res.send(/*html*/ `
+            <li>${newEntry.text}</li>
+        `)
+})
 
+app.delete('/clean', async (req, res) => {
+    const db = await getDb()
+    const name = await db.get('SHOW DATABASES;')
+    console.log('name:', name)
+    // await db.run('DROP DATABASE database;')
+    res.status(200)
+})
 const port = 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)

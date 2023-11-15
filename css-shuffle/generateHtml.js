1da0f4ba664d8850137d1306dde6c86803dfe7d9
romanunr02 <romanunr02@gmail.com>
2023-11-15 22:09:54 +0200

    add function to generate permutations

--- css-shuffle/generateHtml.js
+++ css-shuffle/generateHtml.js
@@ -1,8 +1,22 @@
+let reorders = 0
 function generateReorder(id = crypto.randomUUID()) {
-    const header = document.getElementsByTagName('header')
+    reorders++
     return /*html*/ `
-        <input type="checkbox" name="selected" id="id" hidden/>
-        <label for="${id}">Re Order</label>
+        <input type="checkbox" name="selected" id="${id}" data-current-reorder="${reorders}" hidden/>
+        <label for="${id}" class="absolute inset-0 z-10 cursor-pointer"></label>
+        <style>
+            ${generateReorderCSS(id)}
+        </style>
+    `
+}
+function generateReorderCSS(id) {
+    return /*css*/ `
+        [id="${id}"]:checked + label[for="${id}"]{
+            display:none;
+        }
+        [data-current-reorder="${reorders}"]{
+
+        }
     `
 }
 
@@ -12,4 +26,27 @@ function generateIndicator() {
     return /*html*/ `
         <div class="h-[${100 * currentIndicatorValue}px] w-20 bg-red-500" data-height="${currentIndicatorValue}"></div>
     `
+}
+
+const perms = []
+function* backTrackingPerm() {
+    currentLevel = arguments?.[0] ?? 0
+    for (let i = 1; i <= indicators; i++) {
+        if (perms.includes(i)) continue
+        perms[currentLevel] = i
+        if (currentLevel === indicators - 1) yield [...perms]
+        else yield* backTrackingPerm(currentLevel + 1)
+        delete perms[currentLevel]
+    }
+}
+
+Array.prototype.swap = function (indexA, indexB) {
+    const aux = this[indexA]
+    this[indexA] = this[indexB]
+    this[indexB] = aux
+}
+
+Math.factorial = function (n) {
+    if (n < 1) return 1
+    return n * Math.factorial(n - 1)
 }

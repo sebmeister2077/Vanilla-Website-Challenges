e37a901a10dcc06467fbf2c261355a3e293b9363
romanunr02 <romanunr02@gmail.com>
2023-11-19 17:53:45 +0200

    add toast if updating current user mouse is not allowed

--- chatroom/scripts/firebase/db/mutations/updateUser.js
+++ chatroom/scripts/firebase/db/mutations/updateUser.js
@@ -19,7 +19,22 @@ import {
     limitToLast,
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js'
 import { DATABASE_ROUTES } from '../../../global-vars/index.js'
+import { SECOND_MS } from '../../../constants/time.js'
 
+let lastErrorTime = null
+let lastErrorTimeout = null
 export function updateUser(db, userData) {
-    update(ref(db), { [DATABASE_ROUTES.OneUser(userData.uid)]: userData })
+    if (lastErrorTime !== null || Date.now() - lastErrorTime < 5 * SECOND_MS) return
+
+    update(ref(db), { [DATABASE_ROUTES.OneUser(userData.uid)]: userData }).catch(({ code }) => {
+        if (code === 'PERMISSION_DENIED') {
+            window.toastr.error('Something went wrong')
+            lastErrorTime = Date.now()
+            if (lastErrorTime) clearTimeout(lastErrorTimeout)
+            setTimeout(() => {
+                lastErrorTime = null
+                lastErrorTimeout = null
+            }, 7 * SECOND_MS)
+        }
+    })
 }

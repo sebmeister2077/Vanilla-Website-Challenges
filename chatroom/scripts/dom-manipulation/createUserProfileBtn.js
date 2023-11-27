34626decc65ccdd66374cf38e58211ee38fa85ce
romanunr02 <romanunr02@gmail.com>
2023-11-27 23:39:49 +0200

    add popup with signout button

--- chatroom/scripts/dom-manipulation/createUserProfileBtn.js
+++ chatroom/scripts/dom-manipulation/createUserProfileBtn.js
@@ -3,8 +3,20 @@ export function createUserProfileBtn(photoURL) {
         .html((i, old) => old.trim())
         .contents()
         .clone(true, true)
+    let isOpen = false
     button.on('click', function (e) {
-        console.log(e)
+        const backdrop = button.children('[data-backdrop]')
+        if (isOpen)
+            setTimeout(() => {
+                backdrop.addClass('hidden').addClass('z-[-10]').removeClass('z-10')
+            }, 350)
+        else backdrop.removeClass('hidden').removeClass('z-[-10]').addClass('z-10')
+        requestAnimationFrame(() => {
+            backdrop.toggleClass('bg-transparent').toggleClass('bg-gray-900/25')
+        })
+        button.children('[data-list]').toggleClass('scale-0')
+
+        isOpen = !isOpen
     })
 
     button.children('[data-current-user-img]').attr('src', photoURL)

2e2d4c17a3836f1ea35bbf50b4d084324c4da2e2
romanunr02 <romanunr02@gmail.com>
2024-06-16 16:01:25 +0300

    added a test counter

--- css-timer/generateHtml.js
+++ css-timer/generateHtml.js
@@ -1,25 +1,46 @@
-
+const counterPropertyName = "--counter";
 
 let generatedSegments = 0;
 function generateCounterSegment(secondsInCurrentUnit) {
     const id = crypto.randomUUID();
-    const counterName = `c${++generatedSegments}`
-    return /*html*/`
-    <span id="${id}">
+    const counterName = `c${++generatedSegments}`;
+    return /*html*/ `
+    <span id="${id}" class="segment">
         ${generatedSegments >= 2 ? ":" : ""}
         <style>
             [id="${id}"]::after {
-                counter-set: ${counterName} calc(var(--counter) % ${secondsInCurrentUnit});
+                counter-set: ${counterName} calc(var(${counterPropertyName}) % ${4 || secondsInCurrentUnit});
                 content: counter(${counterName});
             }
         </style>
-    </span>`
+    </span>`;
 }
 
-
-const timer = document.getElementById("timer")
+const maxSignedInt = 100; //Math.pow(2, 31);
+const timer = document.getElementById("timer");
 if (timer) {
-    [60, 1].forEach(secondsInUnit => {
+    window.CSS.registerProperty({
+        name: counterPropertyName,
+        inherits: true,
+        initialValue: 0,
+        syntax: "<integer>",
+    });
+    const timerLogicCSS =
+        /*css */
+        `
+        #timer > span {
+            animation: timer-animation  ${maxSignedInt}s infinite;
+        }
+
+        @keyframes timer-animation {
+            100% {
+                ${counterPropertyName}: ${maxSignedInt};
+            }
+        }
+        `;
+    timer.insertAdjacentHTML("beforeend", /*html*/ `<style>${timerLogicCSS}</style>`);
+
+    [(60, 1)].forEach((secondsInUnit) => {
         timer.insertAdjacentHTML("beforeend", generateCounterSegment(secondsInUnit));
     });
 }
